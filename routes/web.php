9964b8c4fb5f327aa6ff5352e7615c5093de3193
Doniel Acosta <donielacosta1995@gmail.com>
2022-10-04 09:58:45 -0400

    proteccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use  App\Http\Controllers\RepositoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,15 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+  return view('dashboard');
+})->name('dashboard');
+
+
+Route::resource('repositories', RepositoryController::class)->middleware('auth');
